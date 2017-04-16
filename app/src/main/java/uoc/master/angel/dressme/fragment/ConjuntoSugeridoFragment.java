176a00a3109387aa2a40ed338b677240459332e3
andpuerto <andpuerto@hotmail.com>
2017-04-16 12:41:20 +0200

    Tratamiento del JSON del tiempo

--- app/src/main/java/uoc/master/angel/dressme/fragment/ConjuntoSugeridoFragment.java
+++ app/src/main/java/uoc/master/angel/dressme/fragment/ConjuntoSugeridoFragment.java
@@ -17,11 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 
-import org.json.JSONObject;
-
-import uoc.master.angel.dressme.MainActivity;
 import uoc.master.angel.dressme.R;
-import uoc.master.angel.dressme.util.PermissionsUtil;
 import uoc.master.angel.dressme.util.WeatherUtil;
 
 /**
@@ -34,18 +30,23 @@ public class ConjuntoSugeridoFragment extends Fragment{
     //Localizacion mas reciente
     private Location mLastLocation;
 
+    //Para la localizacion
     private LocationManager locationManager;
 
+    //Dialogo a mostrar mientras se consulta el tiempo
+    private ProgressDialog pDialog;
 
-    ProgressDialog pDialog;
+    //Informacion meteorologica
+    private WeatherUtil.WeatherInfo wi = null;
+
     @Override
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        //Creamos un LocationManager con un listener para obtener la localización actual
         locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-        // Define a listener that responds to location updates
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 //Actualizamos la localizacion
@@ -59,7 +60,12 @@ public class ConjuntoSugeridoFragment extends Fragment{
             public void onProviderDisabled(String provider) {}
         };
 
-// Register the listener with the Location Manager to receive location updates
+        //Registramos las actualizaciones tanto a traves de la localizacion de la red, como
+        //del GPS
+        //Dado que no se necesita que la localizacion sea demasiado precisa, puede obtenerse solamente
+        //de la red, con lo que podria quitarse la segunda linea
+        //En las pruebas durante el desarrollo, en el emulador se puede simular el GPS pero no la
+        //localizacion por red, de ahi que registremos en principio ambas.
         try {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
@@ -92,30 +98,32 @@ public class ConjuntoSugeridoFragment extends Fragment{
     }
 
 
-
-
-
-    //******************************************
     /**
-     * Async task class to get json by making HTTP call
+     * Tarea asincrona para obtener la prediccion meteorologica
+     * Dado que requiere contectarse a un servicio a traves de Internet, debe ser asincrona
      */
     private class GetWeather extends AsyncTask<Void, Void, Void> {
 
+        //Antes de la ejecucion, mostramos un dialogo indicando que la tarea esta en curso
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            // Showing progress dialog
             pDialog = new ProgressDialog(getContext());
-            pDialog.setMessage("Please wait...");
+            pDialog.setMessage(getString(R.string.weather_progress));
             pDialog.setCancelable(false);
             pDialog.show();
 
         }
 
+        /**
+         * Tarea a ejecutar
+         */
         @Override
             protected Void doInBackground(Void... arg0) {
 
-                // Making a request to url and getting response
+            //En caso de que no se disponga de la ultima localizacion, intentamos obtenerla
+            //Igual que en el listener, se hace desde los dos proveedores por comodidad durante
+            //el desarrollo. Podria dejarse solamente la localizacion por red.
             if(mLastLocation == null) {
                 try {
                     mLastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
@@ -125,99 +133,42 @@ public class ConjuntoSugeridoFragment extends Fragment{
                 }
             }
             if(mLastLocation != null) {
-                JSONObject jso = WeatherUtil.getWeather(mLastLocation);
-                Log.i("doInbackground","Obtenido el json");
+                //Si tenemos la localizacion, obtenemos la informacion meteorologica
+                wi = WeatherUtil.getWeather(mLastLocation);
+                //El resto lo haremos en onPostExecute
             }else{
-                //error, no se ha podido obtener la localizacion
+                //error: no se ha podido obtener la localizacion
+                Toast.makeText(getContext(), getString(R.string.error_location_connection),
+                        Toast.LENGTH_SHORT).show();
             }
 
-//            Log.e(TAG, "Response from url: " + jsonStr);
-//
-//            if (jsonStr != null) {
-//                try {
-//                    JSONObject jsonObj = new JSONObject(jsonStr);
-//
-//                    // Getting JSON Array node
-//                    JSONArray contacts = jsonObj.getJSONArray("contacts");
-//
-//                    // looping through All Contacts
-//                    for (int i = 0; i < contacts.length(); i++) {
-//                        JSONObject c = contacts.getJSONObject(i);
-//
-//                        String id = c.getString("id");
-//                        String name = c.getString("name");
-//                        String email = c.getString("email");
-//                        String address = c.getString("address");
-//                        String gender = c.getString("gender");
-//
-//                        // Phone node is JSON Object
-//                        JSONObject phone = c.getJSONObject("phone");
-//                        String mobile = phone.getString("mobile");
-//                        String home = phone.getString("home");
-//                        String office = phone.getString("office");
-//
-//                        // tmp hash map for single contact
-//                        HashMap<String, String> contact = new HashMap<>();
-//
-//                        // adding each child node to HashMap key => value
-//                        contact.put("id", id);
-//                        contact.put("name", name);
-//                        contact.put("email", email);
-//                        contact.put("mobile", mobile);
-//
-//                        // adding contact to contact list
-//                        contactList.add(contact);
-//                    }
-//                } catch (final JSONException e) {
-//                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            Toast.makeText(getApplicationContext(),
-//                                    "Json parsing error: " + e.getMessage(),
-//                                    Toast.LENGTH_LONG)
-//                                    .show();
-//                        }
-//                    });
-//
-//                }
-//            } else {
-//                Log.e(TAG, "Couldn't get json from server.");
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(getApplicationContext(),
-//                                "Couldn't get json from server. Check LogCat for possible errors!",
-//                                Toast.LENGTH_LONG)
-//                                .show();
-//                    }
-//                });
-//
-//            }
-//
             return null;
         }
 
+
+        /**
+         * Tras la ejecucion de la tarea
+         */
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
-            // Dismiss the progress dialog
-            if (pDialog.isShowing())
+            //Quitamos el cuadro de dialogo del progreso
+            if (pDialog.isShowing()) {
                 pDialog.dismiss();
-//            /**
-//             * Updating parsed JSON data into ListView
-//             * */
-//            ListAdapter adapter = new SimpleAdapter(
-//                    MainActivity.this, contactList,
-//                    R.layout.list_item, new String[]{"name", "email",
-//                    "mobile"}, new int[]{R.id.name,
-//                    R.id.email, R.id.mobile});
-//
-//            lv.setAdapter(adapter);
+            }
+
+            //Utilizamos la informacion obtenida
+            if(wi != null){
+                //Si la informacion meteorologica se obtiene correctamente, buscaremos
+                //las prendas para el conjunto apropiadas y estableceremos los valores de la vista
+                Log.i("doInbackground","Obtenido el json");
+            } else {
+                //error: no se ha podido consultar la informacion meteorologica
+                Toast.makeText(getContext(), getString(R.string.error_weather),
+                        Toast.LENGTH_SHORT).show();
+            }
 
 
-            //Continuamos ejecutando el algoritmo de creacion del conjunto a partir de los datos
-            //del clima obtenidos
         }
 
     }
--- app/src/main/java/uoc/master/angel/dressme/util/WeatherUtil.java
+++ app/src/main/java/uoc/master/angel/dressme/util/WeatherUtil.java
@@ -4,6 +4,7 @@ package uoc.master.angel.dressme.util;
 import android.location.Location;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,6 +17,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Arrays;
 
 /**
  * Created by angel on 15/04/2017.
@@ -23,24 +25,37 @@ import java.net.URL;
 
 public class WeatherUtil {
 
+    //Tag con el nombre de la clase para los catch
+    private final static String TAG = JSONObject.class.getSimpleName();
+
+    //Lista ORDENADA de codigos de lluvia.
+    private final static int[] rainCodes = {200,201,202,230,231,232,300,301,302,310,311,312,313,314,
+    321,500,501,502,503,504,511,520,521,522,531,600,601,602,611,612,615,616,620,621,622,901,906};
+
     /**
      * Obtiene el tiempo para la localizacion que recibe como parametro
      * @param location La localizacion de la que deseamos saber el tiempo
+     * @return objeto WeatherInfo con la informacion simplificada del tiempo. null en caso de error.
      */
-    public static JSONObject getWeather(Location location){
+    public static WeatherInfo getWeather(Location location){
         //URL del servicio de prediccion del tiempo
         //El parametro appid es necesario para identificar al usuario del servicio. Se obtiene
-        //al darse de alta en el
+        //al darse de alta. Por ultimo, units=metric hace que las temperaturas se obtengan en grados
+        //centigrados en lugar de kelvin, que es la opcion por defecto.
         String reqUrl = "http://api.openweathermap.org/data/2.5/weather?lat="+location.getLatitude()
-                +"&lon="+location.getLongitude()+"&appid=64e41d291ae363c2154323d1253132da";
+                +"&lon="+location.getLongitude()+"&appid=64e41d291ae363c2154323d1253132da&units=metric";
+        //Respuesta en forma de string
         String response = null;
-        String TAG = JSONObject.class.getSimpleName();
+
+        //JSON para almacenar la respuesta
         JSONObject jsonObject = null;
         try {
+            //Obtenemos el objeto URL a partir del string
             URL url = new URL(reqUrl);
+            //Abrimos la conexion, con el metodo get
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            //Leemos la respuesta
+            //Leemos la respuesta y la pasamos al string
             InputStream in = new BufferedInputStream(conn.getInputStream());
             response = convertStreamToString(in);
         } catch (MalformedURLException e) {
@@ -53,20 +68,38 @@ public class WeatherUtil {
             Log.e(TAG, "Exception: " + e.getMessage());
         }
         try {
+            //Creamos el json a partir del string
             jsonObject = new JSONObject(response);
         }catch (JSONException e){
             Log.e(TAG, "JSONException: " + e.getMessage());
         }
 
-        return jsonObject;
+        //Obtenemos el objeto WeatherInfo a partir del json
+        WeatherInfo returnValue = parseWeatherInfo(jsonObject);
+        //Comprobamos si el valor de la temperatura no es valido
+        //En tal caso, devolvemos un null, supondra menos acoplamiento al tratarlo en clases
+        //externas
+        if(returnValue == null || returnValue.temp < -100){
+            return null;
+        }
+        return returnValue;
     }
 
+    /**
+     * Utiliza un InputStream para obtener un String del contenido
+     * @param is InputStream de la fuente
+     * @return String con el contenido obtenido
+     */
     private static String convertStreamToString(InputStream is) {
+        //Creamos un BufferedReader a partir del InputStream
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        //Creamos un String builder
         StringBuilder sb = new StringBuilder();
-
+        //Cada linea que leamos del BufferedReader
         String line;
         try {
+            //Vamos leyendo ineas del BufferedReader las agregamos a la cadena
+            //seguidas de un salto de linea
             while ((line = reader.readLine()) != null) {
                 sb.append(line).append('\n');
             }
@@ -79,7 +112,62 @@ public class WeatherUtil {
                 e.printStackTrace();
             }
         }
+        //Pasamos el StringBuilder a string y lo devolvemos
         return sb.toString();
+    }
+
+
+    /**
+     * Procesa el json recibido y devuelve la informacion relevante simplificada
+     * @param jWeatherInfo El objeto json a procesar
+     * @return WeatherInfo
+     */
+    private static WeatherInfo parseWeatherInfo(JSONObject jWeatherInfo){
+        //Creamos el objeto a devolver
+        WeatherInfo wi = new WeatherUtil().new WeatherInfo();
+
+        try {
+            //Para la informacion de lluvia vamos a utilizar el codigo que proporciona el servicio
+            //El servicio puede devolver varios identificadores de condiciones atmosfericas
+            //Recorremos el array y si alguno es identificativo de lluvia, establecemos la lluvia
+            //a verdadero
+            JSONArray weatherArray = jWeatherInfo.getJSONArray("weather");
+            for(int i=0; i<weatherArray.length(); i++){
+                int weatherCode = weatherArray.getJSONObject(i).getInt("id");
+                //Con el numero de identificador, sabremos si hay lluvia o no
+                //en la documentación de la API, se pueden consultar los valores de codigo
+                //que implican lluvia: https://openweathermap.org/weather-conditions
+                if(Arrays.binarySearch(rainCodes, weatherCode) >= 0){
+                    //Si alguno de los codigos recibidos esta en la lista de los que corresponden
+                    //a lluvia, establecemos la propiedad y terminamos el bucle
+                    wi.lluvia = true;
+                    break;
+                }
+            }
+            //Ahora tomamos la temperatura, en la misma unidad en la que se haya obtenido
+            wi.temp = Float.parseFloat(jWeatherInfo.getJSONObject("main").getString("temp"));
+
+        }catch(JSONException e){
+            Log.e(TAG, "JSONException: " + e.getMessage());
+        }
+
+
+        return wi;
+    }
+
+    /**
+     * Clase interna con informacion resumida del tiempo
+     */
+    public class WeatherInfo{
+        public WeatherInfo(){
+            lluvia = false;
+            //Establecemos la temperatura a un valor no valido para
+            //despues poder saber si se el objeto se ha configurado correctamente
+            temp = -101;
+        }
+
+        public boolean lluvia;
+        public float temp;
     }
 
 }
